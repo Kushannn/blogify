0331fdbc0948ae5dc6342a8f5f4ddce963b5ac26
Kushannn <Kushanrocks.1311@gmail.com>
2024-06-08 14:13:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const path = require("path");
+
+const userRouter = require("./routes/user.js");
+const blogRouter = require("./routes/blog.js");
+const Blog = require("./models/blog.js");
+
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+const { checkForAuthentication } = require("./middlewares/authentication.js");
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+mongoose.connect(process.env.MONGO_URL).then((e) => {
+  console.log("MongoDb connected");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthentication("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  return res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRouter);
+app.use("/blog", blogRouter);
+
+app.listen(PORT, () => `Server Started at ${PORT}`);
